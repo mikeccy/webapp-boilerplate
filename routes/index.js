40bf80b619065d6433310d8f22cf5277cb4655e0
mikeccy <mikeccy@gmail.com>
2013-02-17 12:42:27 -0500

    Changed js indentation

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@
  */
 
 exports.index = function(req, res){
-	var data = { 
-		"title": 'Express with backbone bootstrap less ' + 
-                 req.app.get('view engine') 
-    };
-  	res.render('index', data);
+  var data = { 
+	"title": 'Express with backbone bootstrap less ' + 
+             req.app.get('view engine') 
+  };
+  res.render('index', data);
 };
