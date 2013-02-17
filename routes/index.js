c8fe980d49045e5d1aa0c5a0f8c45db2ae95083d
mikeccy <mikeccy@gmail.com>
2013-02-17 12:37:49 -0500

    Changed template engine to hogan

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express+Backbone+Bootstrap' });
+	var data = { 
+		"title": 'Express with backbone bootstrap less ' + 
+                 req.app.get('view engine') 
+    };
+  	res.render('index', data);
 };
