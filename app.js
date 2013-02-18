699d947ab22755c0ec69e24b5de080ed38bdc29e
mikeccy <mikeccy@gmail.com>
2013-02-18 03:51:10 -0500

    Switched to handlebars for speed and client side templating

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+// setup hbs view engine with partials
+var fs = require('fs')
+  , hbs = require('hbs')
+  , partialsDir = __dirname + '/views/partials/';
+
+hbs.registerPartial('apps', fs.readFileSync(partialsDir + 'apps.hbs',
+                                            'utf8'));
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'hjs');
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -26,10 +34,31 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+  
+  // test precompile
+  // var rawTemplate = '<p>Hello {{name}}</p>{{#if yeah}}<p>Oh {{yeah}}</p>{{/if}}';
+  // var compiledTemplate = hbs.handlebars.precompile(rawTemplate);
+  // console.log(compiledTemplate);
+
+  // setup hbs file to client js precompiler
+  // var hbsPrecompiler = require('handlebars-precompiler');
+  // hbsPrecompiler.watchDir(
+  //   partialsDir,
+  //   __dirname + "/public/js/templates.js",
+  //   ['hbs']
+  // );
+  
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// intercept request
+// app.get('/partials/:file', function(req, res){
+//   var file = req.params.file;
+//   res.type('text/javascript');
+//   res.sendfile(__dirname + '/public/partials/' + file);
+// });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@
  */
 
 exports.index = function(req, res){
-  var data = { 
-	"title": 'Express with backbone bootstrap less ' + 
-             req.app.get('view engine') 
+  var data = {
+    "title": 'Express with backbone bootstrap less ' + 
+             req.app.get('view engine'),
+    "apps": [
+    	{ "name": "Home" },
+    	{ "name": "Profile" },
+    	{ "name": "Chatroom" },
+  	]
   };
+
+  if (req.app.get('env') === 'development') {
+    data['dev'] = true;
+  }
   res.render('index', data);
 };
