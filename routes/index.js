a201e0b4fb4bffb6cdaad6085e5c383e82631e2f
mikeccy <mikeccy@gmail.com>
2013-02-18 18:49:01 -0500

    Generalize app design

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,46 @@ exports.index = function(req, res){
   var data = {
     "title": 'Express with backbone bootstrap less ' + 
              req.app.get('view engine'),
+    "nav": [
+      { 
+        "name": "Home",
+        "href": "http://www.peoi.org/"
+      },
+      { 
+        "name": "Tasks",
+        "href": "#"
+      },
+      { 
+        "name": "Settings",
+        "href": "#"
+      },
+      { 
+        "name": "Logout",
+        "href": "#"
+      },
+    ],
     "apps": [
-    	{ "name": "Home" },
-    	{ "name": "Profile" },
-    	{ "name": "Chatroom" },
+    	{ 
+        "name": "Profile",
+        "href": "#"
+      },
+      { 
+        "name": "Workspace",
+        "href": "https://www.google.com"
+      },
+      { 
+        "name": "Group",
+        "href": "#"
+      },
+    	{ 
+        "name": "Chatroom",
+        "href": "#",
+        "stat": "Beta"
+      },
+      { 
+        "name": "Discussion",
+        "href": "#"
+      },
   	]
   };
 
