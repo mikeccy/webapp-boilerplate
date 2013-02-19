bd4032a1e61bb77c7e67e699c7c86388c325c446
mikeccy <mikeccy@gmail.com>
2013-02-18 19:27:44 -0500

    Minified less files

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(require('less-middleware')({ src: __dirname + '/public',
+                                       compress: true, 
+                                       optimization: 2 }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
